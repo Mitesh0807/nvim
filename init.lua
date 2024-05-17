b784556ec318a59439cc8ddf8280519cf2d79c48
Mitesh0807 <miteshsavaliya7@gmail.com>
2024-05-17 21:29:47 +0530

    neotreee fix

--- init.lua
+++ init.lua
@@ -11,34 +11,4 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 require("vim-options")
-
-
-local plugins = {
- {"catppuccin/nvim", name = "catppuccin", priority = 1000},
- {'nvim-telescope/telescope.nvim', tag = '0.1.6', dependencies = {'nvim-lua/plenary.nvim'}},
- {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"},
- {
-  "nvim-neo-tree/neo-tree.nvim",
-  branch = "v3.x",
-  dependencies = {
-   "nvim-lua/plenary.nvim",
-   "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-   "MunifTanjim/nui.nvim",
-  }
- }
-}
-
-local opts = {}
 require("lazy").setup("plugins")
-local configs = require("nvim-treesitter.configs")
-configs.setup({
- ensure_installed = { "lua", "javascript" },
- highlight = { enable = true },
- indent = { enable = true },  
-}) 
-vim.cmd.colorscheme "catppuccin"
-
-local builtin = require("telescope.builtin")
-vim.keymap.set('n', '<C-p>', builtin.find_files, {})
-vim.keymap.set('n','<leader>fg',builtin.live_grep,{})
-vim.keymap.set('n','<leader>n',':Neotree filesystem reveal float<CR>',{})
