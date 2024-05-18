5110bc1207db72c0a3361061192fc570ec487634
Mitesh0807 <miteshsavaliya7@gmail.com>
2024-05-18 22:38:47 +0530

    lsp-config in progress

---
+++ lua/plugins/lsp-config.lua
@@ -0,0 +1,23 @@
+return {
+  --mason 
+  { "williamboman/mason.nvim",
+    config = function()
+      require("mason").setup()
+    end
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = { "angularls","bashls","css", "dockerls","eslint","graphql","jsonls","html","tsserver"},
+      })
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      local lspconfig = require("lspconfig")
+      lspconfig.lua_ls.setup({})
+    end,
+  }
+}
