cf28f1ff26c3105adb341d73e162e7b75cb75517
Mitesh savaliya <msavaliya@plusonex.com>
2024-05-18 18:53:28 +0530

    neotree catpuccin updated

--- lua/plugins/catpuuccin.lua
+++ lua/plugins/catpuuccin.lua
@@ -5,7 +5,7 @@ return {
     name = "catppuccin",
     priority = 1000,
     config = function()
-      vim.cmd.colorscheme "catppuccin-latte"
+      vim.cmd.colorscheme "catppuccin-mocha"
     end
   }
 }
