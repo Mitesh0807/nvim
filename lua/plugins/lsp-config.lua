e7183d336a1d239da41e45e991f7b75c9144eb0d
Mitesh0807 <miteshsavaliya7@gmail.com>
2024-05-20 22:19:56 +0530

    added lua ls for lsp

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -18,6 +18,7 @@ return {
           "jsonls",
           "html",
           "tsserver",
+          "lua_ls"
         },
       })
     end,
@@ -30,7 +31,7 @@ return {
     },
     lazy = false,
     -- event = "BufReadPre",
-    -- on save format 
+    -- on save format
     config = function()
       local capabilities = require("cmp_nvim_lsp").default_capabilities()
       local lspconfig = require("lspconfig")
