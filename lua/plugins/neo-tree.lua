c4b92d45f30340d451f1c26a63f7cb3b818effc8
Mitesh0807 <miteshsavaliya7@gmail.com>
2024-09-02 18:23:06 +0530

    added the lazyvim setup

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,20 +1,29 @@
 return {
-	"nvim-neo-tree/neo-tree.nvim",
-	branch = "v3.x",
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-		"nvim-tree/nvim-web-devicons",
-		"MunifTanjim/nui.nvim",
-	},
-	config = function()
-    vim.keymap.set('n','<leader>n',':Neotree filesystem reveal float<CR>',{})
-    -- want to open the tree on startup
-    -- but on right side not left side just on startup
-    vim.cmd('Neotree filesystem reveal right')
-    -- now focus the tree on startup
-    -- vim.cmd('Neotree focus')
-    vim.g.neo_tree_remove_legacy_commands = 1
-    -- leader e to get focus
-    vim.keymap.set('n','<leader>e',':Neotree toggle<CR>',{})
-  end,
+  "nvim-neo-tree/neo-tree.nvim",
+  branch = "v3.x",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-tree/nvim-web-devicons",
+    "MunifTanjim/nui.nvim",
+  },
+  --don`t show on startup`
+  opts = {
+    window = {
+      position = "right",
+      mappings = {
+        ["Y"] = "none",
+      },
+    },
+    filesystem = {
+      filtered_items = {
+        hide_dotfiles = false,
+        hide_by_name = {
+          ".git",
+        },
+        always_show = {
+          ".env",
+        },
+      },
+    },
+  },
 }
