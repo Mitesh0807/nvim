620a8fa2ef8835275b83d7f9b9dc3efb94f501e1
Mitesh0807 <miteshsavaliya7@gmail.com>
2025-07-10 07:04:52 +0530

    feat:added print / console statement for ts and golang
    Added a new keymap <leader>cP that inserts a pretty console log
    statement below the current line based on the filetype.
    - For JavaScript/TypeScript: uses JSON.stringify for readable object
    output  e.g., console.log('user:', JSON.stringify(user, null, 2));
    - For Go: inserts fmt.Printf("%+v\n", variable)
    - For others: falls back to a basic console.log('variable:',
    variable)Improves debugging productivity by providing better structured
    log output.

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,24 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+vim.keymap.set("n", "<leader>cP", function()
+  local word = vim.fn.expand("<cword>")
+  if word == "" then
+    return
+  end
+
+  local filetype = vim.bo.filetype
+  local log_line = ""
+
+  if filetype:match("typescript") or filetype:match("javascript") then
+    log_line = string.format("console.log('%s:', JSON.stringify(%s, null, 2));", word, word)
+  elseif filetype == "go" then
+    log_line = string.format('fmt.Printf("%%+v\\n", %s)', word)
+  else
+    log_line = string.format("console.log('%s:', %s);", word, word)
+  end
+
+  local row = vim.api.nvim_win_get_cursor(0)[1]
+  vim.api.nvim_buf_set_lines(0, row, row, false, { log_line })
+end, { desc = "Pretty console log under cursor (by filetype)" })
---
+++ lua/plugins/hardtime.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "m4xshen/hardtime.nvim",
+    lazy = false,
+    dependencies = { "MunifTanjim/nui.nvim" },
+    opts = {
+      -- Optional: customize settings here
+      -- disable_mouse = false,
+      -- restriction_mode = "hint",
+    },
+  },
+}
